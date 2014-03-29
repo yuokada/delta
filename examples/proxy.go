59a542ca4e73d133941b449b23fd3145a99ffb69
Yukihiro Okada <callistoiv+git@gmail.com>
2014-03-29 12:55:40 +0900

    Update proxy.go

--- examples/proxy.go
+++ examples/proxy.go
@@ -1,37 +1,47 @@
 package main
 
 import (
-	"../"
-	"log"
-	"net/http"
-	"time"
+        "../"
+        "fmt"
+        "log"
+        "net/http"
+        "time"
 )
 
 func main() {
-	server := delta.NewServer("0.0.0.0", 8484)
+        // 8484ポートでdeltaがLISTEN
+        server := delta.NewServer("0.0.0.0", 8484)
+        //既存サーバー?
+        server.AddMasterBackend("production", "127.0.0.1", 8080)
+        //新サーバー?
+        server.AddBackend("testing", "127.0.0.1", 8081)
 
-	server.AddMasterBackend("production", "127.0.0.1", 8080)
-	server.AddBackend("testing", "127.0.0.1", 8081)
+        server.OnSelectBackend(func(req *http.Request) []string {
+                // GETリクエストならproductionとtestingの両方にリクエストを送るが
+                // それ以外はproductionのみに送る
+                if req.Method == "GET" {
+                        return []string{"production", "testing"}
+                } else {
+                        return []string{"production"}
+                }
+        })
 
-	server.OnSelectBackend(func(req *http.Request) []string {
-		if req.Method == "GET" {
-			return []string{"production", "testing"}
-		} else {
-			return []string{"production"}
-		}
-	})
+        server.OnMungeHeader(func(backend string, header *http.Header) {
+                if backend == "testing" {
+                        header.Add("X-Delta-Sandbox", "1")
+                }
+        })
 
-	server.OnMungeHeader(func(backend string, header *http.Header) {
-		if backend == "testing" {
-			header.Add("X-Delta-Sandbox", "1")
-		}
-	})
+        server.OnBackendFinished(func(responses map[string]*delta.Response) {
+                for backend, response := range responses {
+                        log.Printf("%s [%d ms]: ", backend, (response.Elapsed / time.Millisecond))
+                        for k, v := range response.HttpResponse.Header {
+                                //log.Printf("%s:  %+V\n", k, v)
+                                fmt.Printf("%s: %s\n", k, v[0])
+                        }
+                        log.Printf("\n%s", response.Data)
+                }
+        })
 
-	server.OnBackendFinished(func(responses map[string]*delta.Response) {
-		for backend, response := range responses {
-			log.Printf("%s [%d ms]: %s", backend, (response.Elapsed / time.Millisecond), response.Data)
-		}
-	})
-
-	server.Run()
+        server.Run()
 }
